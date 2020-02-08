74c2f1401973c9e5df62631306c62faaf168434a
Mia <miahljnh@gigaming.io>
2020-02-08 19:46:17 +0700

    .

---
+++ Task4_BootcampRefactory/Cart.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task4_BootcampRefactory
+{
+    public class Cart: ICart
+    {
+
+    }
+}
---
+++ Task4_BootcampRefactory/ICart.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Task4_BootcampRefactory
+{
+    public interface ICart
+    {
+        ICart AddItem(int item_id, int price, int quantity = 1);
+        ICart RemoveItem(int item_id);
+        ICart AddDiscount(string disc);
+    }
+}
